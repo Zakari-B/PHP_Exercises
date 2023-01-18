f7c788b9d1e16f2d9956e3e73aa1a8cde5af6325
Zakari Boureghda <zakari.boureghda@gmail.com>
2023-01-18 15:21:48 +0100

    Added an homepage for the categories

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/assets/style.css" media="screen" rel="stylesheet" type="text/css" />
+    <title>Exercises homepage</title>
+</head>
+
+<body>
+    <a href="./Archeology/archeology_index.php">Head to the archeological site !</a>
+    <a href="./Garden/garden_index.php">Go to the garden.</a>
+</body>
+
+</html>
