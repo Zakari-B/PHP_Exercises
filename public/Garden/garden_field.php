d212fbdb22da8a2cba29decfa02187e8cdab1fd4
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.fr>
2023-03-15 15:07:23 +0100

    fix style

--- public/Garden/garden_field.php
+++ public/Garden/garden_field.php
@@ -24,6 +24,7 @@ $garden = $gardens[$stage] ?? [];
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="../assets/exercise.css" media="screen" rel="stylesheet" type="text/css" />
   <link href="/Garden/assets/field_style.css" media="screen" rel="stylesheet" type="text/css" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- public/Garden/garden_planter.php
+++ public/Garden/garden_planter.php
@@ -51,6 +51,7 @@ if (!in_array($plants[$stage]['pot'], $allowedList)) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="../assets/exercise.css" media="screen" rel="stylesheet" type="text/css" />
     <link href="/Garden/assets/planter_style.css" media="screen" rel="stylesheet" type="text/css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -66,62 +67,61 @@ if (!in_array($plants[$stage]['pot'], $allowedList)) {
             <h1>The planter</h1>
             <a id="back" href="/Garden/"><img src="../assets/img/back.png" alt="Home icon" /></a>
         </header>
-        <main class="exercise">
 
-            <div class="content">
-                <div class="planter border">
-                    <div class="soil">
-                        <?php for ($i = 0; $i < 5 && $planterIsInvalid === false; $i++) : ?>
-                            <div class="planter-plot">
-                                <?= $plants[$stage]['planter'][$i] ?? '' ?>
-                            </div>
-                        <?php endfor; ?>
+        <main class="exercise">
+            <?php if (isset($errors)) : ?>
+                <div class="error-container">
+                    <div id="error">
+                        <?php foreach ($errors as $error) : ?>
+                            <p><?= $error ?></p>
+                        <?php endforeach; ?>
                     </div>
                 </div>
-                <div class="pot border">
-                    <div class="soil soil-pot">
-                        <?php if (in_array($plants[$stage]['pot'], $allowedList)) : ?>
-                            <?= $plants[$stage]['pot'] ?? ''; ?>
-                        <?php endif; ?>
+            <?php endif; ?>
+
+            <div class="content">
+                <div class="planter">
+                    <?php for ($i = 0; $i < 5 && $planterIsInvalid === false; $i++) : ?>
+                        <div class="plot">
+                            <?= $plants[$stage]['planter'][$i] ?? '' ?>
+                        </div>
+                    <?php endfor; ?>
+                </div>
+
+                <div class="planter pot">
+                    <div class="plot">
+                        <?= $plants[$stage]['pot'] ?? ''; ?>
                     </div>
                 </div>
             </div>
 
             <div class="instructions">
-                <?php if (isset($errors)) : ?>
-                    <div class="error-container">
-                        <div id="error">
-                            <?php foreach ($errors as $error) : ?>
-                                <p><?= $error ?></p>
-                            <?php endforeach; ?>
-                        </div>
-                    </div>
-                <?php endif; ?>
+
                 <div class="instructions-top">
                     <h2>Stage <?= $stage ?></h2>
+
                     <div class="nav-btn">
                         <?php if ($stage > 1) : ?>
-                            <a href="?stage=<?= $stage - 1 ?>"> <- Prev</a>
+                            <a href="?stage=<?= $stage - 1 ?>"> < Prev</a>
                                 <?php endif; ?>
                                 <?php if ($stage < count($instructions)) : ?>
-                                    <a href="?stage=<?= $stage + 1 ?>"> Next -> </a>
+                                    <a href="?stage=<?= $stage + 1 ?>"> Next > </a>
                                 <?php endif; ?>
                     </div>
                 </div>
                 <p><?= nl2br($instructions[$stage]["desc"]) ?></p>
+
                 <div class="expectations">
                     <p>Expected : </p>
-                    <div class="mini-planter border">
-                        <div class="soil">
-                            <?php foreach ($instructions[$stage]["result"]["planter"] as $plant) : ?>
-                                <div class="mini-planter-plot">
-                                    <?= $plant ?>
-                                </div>
-                            <?php endforeach ?>
-                        </div>
+                    <div class="planter mini">
+                        <?php foreach ($instructions[$stage]["result"]["planter"] as $plant) : ?>
+                            <div class="plot">
+                                <?= $plant ?>
+                            </div>
+                        <?php endforeach ?>
                     </div>
-                    <div class="mini-pot border">
-                        <div class="soil soil-pot">
+                    <div class="planter mini pot">
+                        <div class="plot">
                             <?php echo $instructions[$stage]["result"]["pot"] ?>
                         </div>
                     </div>
